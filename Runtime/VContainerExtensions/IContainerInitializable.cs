d7e30efe1aa06b6c2059aa75e9477614e273e03f
takahashi1682 <h.takahashi@morijyobi.ac.jp>
2026-05-29 08:30:44 +0900

    アセンブリ修正

---
+++ Runtime/VContainerExtensions/IContainerInitializable.cs
@@ -0,0 +1,19 @@
+﻿using VContainer;
+
+namespace MyUtils.VContainerExtensions
+{
+    public interface IContainerInitializable
+    {
+        /// <summary>
+        /// 依存性注入の登録時に呼び出される初期化処理
+        /// </summary>
+        /// <param name="builder"></param>
+        void OnRegister(IContainerBuilder builder);
+
+        /// <summary>
+        /// 依存性注入の解決時に呼び出される初期化処理
+        /// </summary>
+        /// <param name="resolver"></param>
+        void OnResolve(IObjectResolver resolver);
+    }
+}
