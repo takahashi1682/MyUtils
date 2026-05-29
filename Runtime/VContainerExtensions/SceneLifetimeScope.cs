e6a1c22b5fbc4ed7547042c860ac51c2292b5539
takahashi1682 <h.takahashi@morijyobi.ac.jp>
2026-05-29 09:21:42 +0900

    ファイル移動

---
+++ Runtime/VContainerExtensions/SceneLifetimeScope.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using VContainer;
+using VContainer.Unity;
+
+namespace MyUtils.VContainerExtensions
+{
+    /// <summary>
+    /// シーン固有のコンテナ初期化プロセスを定義するインターフェース
+    /// </summary>
+    public interface ISceneInitializable : IContainerInitializable
+    {
+    }
+
+    /// <summary>
+    /// 全てのシーンに配置するLifetimeScope。
+    /// シーン内のISceneContainerInitializableを自動検出し、独立したコンテナ構築フローを提供します。
+    /// </summary>
+    public class SceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            var targets = new List<ISceneInitializable>();
+            var rootObjects = gameObject.scene.GetRootGameObjects();
+
+            foreach (var obj in rootObjects)
+            {
+                targets.AddRange(obj.GetComponentsInChildren<ISceneInitializable>(true));
+            }
+
+            builder.RegisterBuildCallback(resolver =>
+            {
+                var scopedResolver = resolver.CreateScope(newBuilder =>
+                {
+                    foreach (var target in targets)
+                    {
+                        target.OnRegister(newBuilder);
+                    }
+                });
+
+                foreach (var target in targets)
+                {
+                    target.OnResolve(scopedResolver);
+                }
+            });
+        }
+    }
+}
